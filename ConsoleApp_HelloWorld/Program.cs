27abecbafb86a7061e175c469c85e4795afcc9d7
didrika@protonmail.com <didrika@protonmail.com>
2020-09-07 01:14:44 -0700

    ADded A 2

--- ConsoleApp_HelloWorld/Program.cs
+++ ConsoleApp_HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp_HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helloa World!");
+            Console.WriteLine("Helloa World 2!");
         }
     }
 }
